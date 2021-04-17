6d2858e6163f640676853568118459e889b0a229
rubin16 <rubin16@users.noreply.github.com>
2021-04-17 19:09:45 +0300

    Update report13.sql

--- scripts/report13.sql
+++ scripts/report13.sql
@@ -1,9 +1,9 @@
 /**
  * List files on Wikipedia and Commons that share a title but are not the same file.
- * Author: Fastily
+ * Author: Fastily (forked)
  */
 SELECT wpimg.img_name
-FROM enwiki_p.image wpimg
+FROM ruwiki_p.image wpimg
 INNER JOIN commonswiki_p.image cimg
 ON wpimg.img_name = cimg.img_name
 WHERE cimg.img_sha1 <> wpimg.img_sha1;
